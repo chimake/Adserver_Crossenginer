640325449fe3dfe73b69ace4b7baae7ef4cf2d04
chimake <dev.chimaobi@gmail.com>
2019-12-29 03:28:38 -0500

    Admin ui fix

--- application/views/admin/header_view.php
+++ application/views/admin/header_view.php
@@ -1,107 +1,174 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html>
-<title><?php echo $title; ?></title>
-<meta charset="UTF-8">
-<link rel="icon" href="<?php echo base_url('assets/media/images/favicon.png'); ?>" type="image/x-icon"/>
+<head>
+    <title><?php echo $title; ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="<?php echo base_url('assets/media/images/favicon.png'); ?>" rel="shortcut icon">
 
-<meta name="description" content="">
-<meta name="keywords" content="<?php
-//echo $keywords;
-?>">
+    <meta name="description" content="<?php echo $description; ?>">
+    <meta name="keywords" content="<?php
+    echo $keywords;
+    ?>">
+    <meta property="og:image" content="<?php
 
-
-<meta property="og:image" content="<?php
-
-echo base_url('assets/media/images/pricetagfav.ico');
-
-
-
-?>
+    echo base_url('assets/media/images/faviconsocial.png');
+    ?>
 " />
+    <meta property="og:description" content="<?php echo $description; ?>" />
+    <meta property="og:url"content="<?php echo current_url(); ?>" />
+    <meta property="og:title" content="<?php echo $title; ?>" />
+    <meta name="author" content="<?php echo $author;?>">
+    <link rel="stylesheet"  href="<?php echo base_url('assets/mdi/css/materialdesignicons.min.css'); ?>"/>
 
-<meta property="og:description" content="<?php //echo $description; ?>" />
+    <link rel="stylesheet"  href="<?php echo base_url('assets/cj/css/vendor.bundle.base.css'); ?>"/>
 
-<meta property="og:url"content="<?php //echo current_url(); ?>" />
+    <link href="<?php echo base_url('assets/cj/css/style.css'); ?>" rel="stylesheet">
+    <!-- End layout styles -->
+</head>
 
-<meta property="og:title" content="<?php //echo $title; ?>" />
-
-
-<meta name="author" content="<?php //echo $author;?>">
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"  href="<?php echo base_url('assets/cj/fontawesome-4.6.3.min.css'); ?>"/>
-
-<link rel="stylesheet"  href="<?php echo base_url('assets/cj/w3mobile.css'); ?>"/>
-
-
-<link rel="stylesheet"  href="<?php echo base_url('assets/cj/w3.css'); ?>"/>
-
-<!--<link rel="stylesheet"  href="<?php echo base_url('assets/cj/w3mobile.css'); ?>">-->
-
-
-		<!---editor dependency plus jquery-->
-  <link rel="stylesheet"  href="<?php echo base_url('assets/bootstrap3.3.5/css/bootstrap.css'); ?>">
-  <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.js'); ?>">
-		</script>
-  <script type="text/javascript" src="<?php echo base_url('assets/bootstrap3.3.5/js/bootstrap.js'); ?>">
-		</script>
-<link rel="stylesheet"  href="<?php echo base_url('assets/dist/summernote.css'); ?>">
-<script type="text/javascript" src="<?php echo base_url('assets/dist/summernote.js'); ?>">
-
-</script>
-
-<style>
-a {
-  text-decoration: none !important;
-}
-
-@media screen and (min-width:400px){
-#menuc {
-width:50%;
-}
-#imgmedia {
-display:inline-block;
-width:40%;
-height:50%;
-}
-}
-
-
-
-@media screen and (max-width:400px){
-
-
-
-}
-
-
-
-
-
-
-
-a {
-text-decoration:none;
-}
-
-body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
-.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
-.fa-anchor,.fa-coffee {font-size:200px}
-</style>
 <body>
+<div class="container-scroller">
+    <!-- Navbar -->
+    <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+        <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+            <a class="navbar-brand brand-logo" href="<?= site_url('advertiser_dashboard');?> "><img src="<?= base_url('assets/media/images/logo.svg'); ?>" alt="logo" /></a>
+            <a class="navbar-brand brand-logo-mini" href="<?= site_url('advertiser_dashboard');?>"><img src="<?= base_url('assets/media/images/logo-mini.svg'); ?>" alt="logo" /></a>
+        </div>
+        <div class="navbar-menu-wrapper d-flex align-items-stretch">
+            <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+                <span class="mdi mdi-menu"></span>
+            </button>
 
-<div style="height:2%;width:100%;padding:1%;" class="w3-bar w3-black w3-text-white">
-<div style="height:auto;width:20%;padding:;display:inline" class="w3-bar w3-black w3-text-white">
-</div>
+            <ul class="navbar-nav navbar-nav-right">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        Account Balance : <b><?= $account_bal ?></b>
+                    </a>
+                </li>
+                <li class="nav-item nav-profile dropdown">
+                    <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+                        <div class="nav-profile-img">
+                            <img src="<?= base_url('assets/media/images/faces/face1.jpg'); ?>" alt="image">
+                            <span class="availability-status online"></span>
+                        </div>
+                    </a>
+                    <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
+                        <a class="dropdown-item" href="<?= site_url("Logout"); ?>">
+                            <i class="mdi mdi-logout mr-2 text-primary"></i> Signout </a>
+                    </div>
+                </li>
 
 
-<span class="w3-border w3-border-white w3-padding">
-Custch Advertisment<br><br>
-</span>
+                <li class="nav-item dropdown">
+                    <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
+                        <i class="mdi mdi-bell-outline"></i>
+                        <span class="count-symbol bg-danger"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+                        <h6 class="p-3 mb-0">Notifications</h6>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item preview-item">
+                            <div class="preview-thumbnail">
+                                <div class="preview-icon bg-success">
+                                    <i class="mdi mdi-calendar"></i>
+                                </div>
+                            </div>
+                            <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                <h6 class="preview-subject font-weight-normal mb-1">Event today</h6>
+                                <p class="text-gray ellipsis mb-0"> Just a reminder that you have an event today </p>
+                            </div>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item preview-item">
+                            <div class="preview-thumbnail">
+                                <div class="preview-icon bg-warning">
+                                    <i class="mdi mdi-settings"></i>
+                                </div>
+                            </div>
+                            <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                <h6 class="preview-subject font-weight-normal mb-1">Settings</h6>
+                                <p class="text-gray ellipsis mb-0"> Update dashboard </p>
+                            </div>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item preview-item">
+                            <div class="preview-thumbnail">
+                                <div class="preview-icon bg-info">
+                                    <i class="mdi mdi-link-variant"></i>
+                                </div>
+                            </div>
+                            <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                                <h6 class="preview-subject font-weight-normal mb-1">Launch Admin</h6>
+                                <p class="text-gray ellipsis mb-0"> New admin wow! </p>
+                            </div>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <h6 class="p-3 mb-0 text-center">See all notifications</h6>
+                    </div>
+                </li>
 
-</div>
+
+            </ul>
+            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+                <span class="mdi mdi-menu"></span>
+            </button>
+        </div>
+    </nav>
+
+    <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        <nav class="sidebar sidebar-offcanvas" id="sidebar">
+            <ul class="nav">
+                <li class="nav-item nav-profile">
+                    <a href="#" class="nav-link">
+                        <div class="nav-profile-image">
+                            <img src="<?= base_url('assets/media/images/faces/face1.jpg'); ?>" alt="profile">
+                            <span class="login-status online"></span>
+                            <!--change to offline or busy as needed-->
+                        </div>
+                        <div class="nav-profile-text d-flex flex-column">
+                            <span class="font-weight-bold mb-2"><?= $user['firstname']." ".$user['lastname']; ?></span>
+                            <span class="text-secondary text-small"><?= $user['account_status'];?></span>
+                        </div>
+                        <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= site_url('advertiser_dashboard');?>">
+                        <span class="menu-title">Dashboard</span>
+                        <i class="mdi mdi-home menu-icon"></i>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= site_url("advertiser_dashboard/Campaign") ?>">
+                        <span class="menu-title">Campaign</span>
+                        <i class="mdi mdi-bell-ring-outline menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= site_url("advertiser_dashboard/cpa_forms_list") ?>">
+                        <span class="menu-title">Forms</span>
+                        <i class="mdi mdi-clipboard-text menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= site_url("advertiser_dashboard/payment") ?>"">
+                    <span class="menu-title">Payments</span>
+                    <i class="mdi mdi-wallet menu-icon"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= site_url('advertiser_dashboard/settings') ?>">
+                        <span class="menu-title">Settings</span>
+                        <i class="mdi mdi-settings menu-icon"></i>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- partial -->
 
 
-<div class=""><!--container div-->
+        <noscript>Pls turn on JavaScript!</noscript>
--- application/views/admin/login_view.php
+++ application/views/admin/login_view.php
@@ -1,29 +1,98 @@
-<br><br><br>
-<div class="w3-container w3-text-indigo w3-center w3-margin-top"><?php
-echo validation_errors();
-if(isset($_SESSION['err_msg']))
-{
-  echo $_SESSION['err_msg'];
-} ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div style="width:100%" class="w3-container">
-<?php echo form_open("/ch_admin");
+<head>
+    <title><?php echo $title; ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="<?php echo base_url('assets/media/images/favicon.png'); ?>" rel="shortcut icon">
 
-?>
-        <center>
-        	<span class="w3-text-indigo w3-large">ADmin Login</span><br>
+    <meta name="description" content="<?php echo $description; ?>">
+    <meta name="keywords" content="<?php
+    echo $keywords;
+    ?>">
+    <meta property="og:image" content="<?php
 
-<span class="w3-label">Username:</span>
+    echo base_url('assets/media/images/faviconsocial.png');
+    ?>
+" />
+    <meta property="og:description" content="<?php echo $description; ?>" />
+    <meta property="og:url"content="<?php echo current_url(); ?>" />
+    <meta property="og:title" content="<?php echo $title; ?>" />
+    <meta name="author" content="<?php echo $author;?>">
+    <link rel="stylesheet"  href="<?php echo base_url('assets/mdi/css/materialdesignicons.min.css'); ?>"/>
 
-<input  style="width:60%" class="w3-input" name="name" value="<?php echo set_value("name"); ?>" placeholder="Username" requiindigo></input>
-<br>
+    <link rel="stylesheet"  href="<?php echo base_url('assets/cj/css/vendor.bundle.base.css'); ?>"/>
 
-<span class="w3-label">Password:</span>
-<input style="width:60%" class="w3-input" type='password' name="pass" value="<?php echo set_value("pass"); ?>" placeholder="Password" requiindigo></input>
-<br>
+    <link href="<?php echo base_url('assets/cj/css/style.css'); ?>" rel="stylesheet">
+    <!-- End layout styles -->
+</head>
+<body>
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth">
+            <div class="row flex-grow">
+                <div class="col-lg-4 mx-auto">
+                    <div class="auth-form-light text-left p-5">
+                        <div class="brand-logo">
+                            <img src="<?=base_url("assets/media/images/logo.png") ?>">
+                        </div>
+                        <h4>Hello! let's get started</h4>
+                        <h6 class="font-weight-light">Sign in to continue.</h6>
+                        <div class="text-danger">
+                            <?php echo validation_errors();
+                            if(isset($_SESSION['err_msg']))
+                            {
 
-<input class="w3-btn w3-indigo" name="submit" type="submit" value="Login"></center>
-</div></div>
+                                echo $_SESSION['err_msg'];
+
+                            }
+                            ?>
+                        </div>
+                        <div class="text-danger">
+                            <?php if(isset($_SESSION['action_status_report']))
+                            {
+
+                                echo $_SESSION['action_status_report'];
+
+                            }
+                            ?>
+                        </div>
+                        <form class="pt-3" method="post" action="<?php echo site_url('/ch_admin'); ?>">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-lg" value="<?php echo set_value("name"); ?>" name="name" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-lg" id="inputPassword" name="pass" value="<?php echo set_value("pass"); ?>" placeholder="Password">
+                            </div>
+                            <div class="mt-3">
+                                <input class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" type='submit' name='submit' value="Sign In">
+                            </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+<!-- plugins:js -->
+<script src="<?php echo base_url('assets/js/vendor.bundle.base.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/off-canvas.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/hoverable-collapse.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/misc.js'); ?>"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+
+<!-- endinject -->
+</body>
+</html>
+
 
 
 
